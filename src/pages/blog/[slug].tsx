98a4f37f4e9e8b0d4e59eb0d16623aa020b019e4
Alex Gogl <alex@gogl.io>
2020-12-16 09:02:52 +0100

    add initial blog

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,30 +1,46 @@
+import Navbar from "@components/Navbar";
 import client from "client";
-import { GetStaticProps } from "next";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { groq } from "next-sanity";
 import { getClient } from "sanity";
 
-function Blog({ post }) {
-  return <div>Test {JSON.stringify(post)}</div>;
+interface Post {
+  title: string;
+}
+
+function Blog({ post }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      <Navbar borderShown />
+      <main className="container prose">
+        <h1>{post.title}</h1>
+      </main>
+    </div>
+  );
 }
 
 export async function getStaticProps({ params }) {
-  const post = await getClient(params.slug).fetch(
+  const post: Post = await getClient(false).fetch(
     groq` *[_type == "post" && slug.current == $slug][0]`,
     {
       slug: params.slug,
     }
   );
 
-  console.log(post);
-
   return { props: { post } };
 }
 
 export async function getStaticPaths() {
+  const queryAll = groq`*[_type == "post" && slug.current != ''] {
+    'slug': slug.current
+  }`;
+
+  const pages: { slug: string }[] = await getClient(false).fetch(queryAll);
+
+  const generateList = pages.map(({ slug }) => ({ params: { slug } }));
+
   return {
-    paths: [
-      { params: { slug: "test" } }, // See the "paths" section below
-    ],
+    paths: generateList,
     fallback: true,
   };
 }
