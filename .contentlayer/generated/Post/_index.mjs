54182a1254cdc8ef9970c7f286dea00bf4ad0a1d
Alex Gogl <alex@gogl.io>
2022-04-29 21:07:08 +0200

    more text

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -2,6 +2,8 @@
 
 import definitionOfAStartupMdx from './definition-of-a-startup.mdx.json'
 import lifeExperienceMdx from './life-experience.mdx.json'
+import measureIdeasMdx from './measure-ideas.mdx.json'
 import startupLearningsMdx from './startup-learnings.mdx.json'
+import powerOfImaginationMdx from './power-of-imagination.mdx.json'
 
-export const allPosts = [definitionOfAStartupMdx, lifeExperienceMdx, startupLearningsMdx]
+export const allPosts = [definitionOfAStartupMdx, lifeExperienceMdx, measureIdeasMdx, startupLearningsMdx, powerOfImaginationMdx]
